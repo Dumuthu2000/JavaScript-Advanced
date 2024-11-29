0ba23a14abf7cafd7686cd45e2c2b9c0667d5e02
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-29 16:24:32 +0530

    Create a blocking task

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Bdud a, nksndaskdskadnaskdnda")
+//Blocking task
+function blockFunc(){
+    for (let i = 0; i < 5e9; i++) {};
+    console.log("Finished long task");
+};
+
+console.log("Start");
+blockFunc();
+console.log("End");
