75e9d4e5d7fa4dfc02021eeae397685346fa832d
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-29 16:52:11 +0530

    Add callback asynchronous function

---
+++ server.js
@@ -0,0 +1,19 @@
+//Normal function
+const sayHello = (name, callback) => {
+    console.log(`Hi this is ${name}`);
+
+    //Invoking the callback function
+    setTimeout(()=>{callback()},2000);
+};
+
+//Initialize the callback function
+function sayBye(){
+    for (let i = 0; i < 2e9; i++) {}
+    console.log("End callback");
+    
+};
+
+console.log("Start program");
+//Invoking sayHello function
+sayHello("1 Billion Tech", sayBye);
+console.log("End program");
