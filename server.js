439a4cad24cdabba6866234faa7d5c813045abd2
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-12-02 13:16:13 +0530

    Add inheritance

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-class Car{
+class Vehicle{
     #engineStatus;
+    _price;
 
     constructor(brand, model){
         this.brand = brand;
@@ -17,6 +18,20 @@ class Car{
     }
 }
 
-let myCar = new Car("Toyota", "Prius");
-myCar.startEngine();
-console.log(myCar.getEngineStatus());
+//Inheritance
+class Car extends Vehicle{
+    constructor(brand, model, isRegeistered){
+        super(brand, model);
+        this.isRegeistered = isRegeistered;
+    }
+
+    buyCar(){
+        console.log(`I was buy a ${this.brand} ${this.model} and that is registered ${this.isRegeistered}`);
+    }
+}
+
+// let myVehicle = new Vehicle("Toyota", "Prius");
+// myVehicle.startEngine();
+// console.log(myVehicle.getEngineStatus());
+let myCar = new Car("Honda", "Civic", true);
+myCar.buyCar();
